44132bfd79d7c072aac168744b4b76719dc54451
karanagi <karanagi@wlp1s0.net>
2026-07-02 13:39:14 -0700

    Added new project `shift_register_pwm`

---
+++ shift_register_pwm/src/main.c
@@ -0,0 +1,18 @@
+#define F_CPU	1000000
+
+#include </usr/avr/include/avr/io.h>
+#include </usr/avr/include/util/delay.h>
+
+#define SLEEP_TIME		300
+
+/* 
+ * PB2: SER
+ * PB1: RCLK
+ * PB0: SRCLK
+ */
+
+int
+main(void)
+{
+}
+
